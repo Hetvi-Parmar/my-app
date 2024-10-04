454b3b6cac08b3adfb133abe55135abd394dbb75
Parmar Hetvi <hetvi9434@gamil.com>
2024-10-04 14:51:07 +0530

    Fixed line ending issues and removed submodule

--- src/Component/NewsItem.js
+++ src/Component/NewsItem.js
@@ -1,23 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class NewsItem extends Component {
     render() {
-        
-        let {title, description, imageUrl, newsUrl} = this.props;  
-        
+        // Destructuring props to get title, description, imageUrl, and newsUrl
+        let { title, description, imageUrl, newsUrl } = this.props;
+
         return (
             <div className="my-3">
-                <div className="card" style={{width: "18rem"}}>
-                    <img src={!imageUrl ? "https://regmedia.co.uk/2023/07/14/india_shutterstock.jpg" : imageUrl} className="card-img-top" alt="..." />
-                    
+                <div className="card" style={{ width: "18rem" }}>
+                    {/* Default image if imageUrl is not provided */}
+                    <img 
+                        src={imageUrl ? imageUrl : "https://regmedia.co.uk/2023/07/14/india_shutterstock.jpg"} 
+                        className="card-img-top" 
+                        alt="News" 
+                    />
+
                     <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
-                        <p className="card-text">{description}...</p>
-                        <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark">Read More</a>  
+                        {/* Ensure there's no additional ellipsis if text is already sliced */}
+                        <h5 className="card-title">
+                            {title ? title + '...' : ''}
+                        </h5>
+
+                        <p className="card-text">
+                            {description ? description + '...' : ''}
+                        </p>
+
+                        <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark">
+                            Read More
+                        </a>
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
